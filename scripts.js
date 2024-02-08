8af6fb71c9b3d059929fe666b6261bb6a6cd9603
Fuad Ismail <fuad1502@gmail.com>
2024-02-08 15:37:44 +0700

    Finished signup HTML CSS and JS, except for the validation

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function showSignup() {
+  document.getElementById('signup').style.display = 'flex';
+  document.getElementById('login').style.display = 'none';
+}
+
+function showLogin() {
+  document.getElementById('signup').style.display = 'none';
+  document.getElementById('login').style.display = 'flex';
+}

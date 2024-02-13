70e88f1fa4234a908fd826d3d0d1d64350c9fd1f
Fuad Ismail <fuad1502@gmail.com>
2024-02-13 13:41:44 +0700

    Added back OPTION HTTP request handling in CORSMiddleware

--- backend/gin-middlewares.go
+++ backend/gin-middlewares.go
@@ -29,6 +29,11 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
 		c.Next()
 	}
 }

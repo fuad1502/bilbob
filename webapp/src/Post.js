bbeac4604072feb4495b2d99b642b1d8d8562e0a
Fuad Ismail <fuad1502@gmail.com>
2024-02-17 16:34:01 +0700

    Add date to Post React component

--- webapp/src/Post.js
+++ webapp/src/Post.js
@@ -2,10 +2,30 @@ import React from "react";
 import './Post.css'
 
 export default function Post ({post}) {
+  const postDateString = displayPostDate(post.postDate);
   return (
     <div id="post">
-      <p id="username">{'@' + post.username}</p>
+      <p id="username">{'@' + post.username} · {postDateString}</p>
       <p id="postText">{post.postText}</p>
     </div>
   )
+}
+
+function displayPostDate (postDate) {
+  const timestamp = Date.parse(postDate);
+  const secondsAgo = (Date.now() - timestamp) / 1000;
+  const minutesAgo = secondsAgo / 60;
+  const hoursAgo = minutesAgo / 60;
+  const daysAgo = hoursAgo / 24;
+  if (secondsAgo < 60) {
+    return 'now';
+  } else if (minutesAgo < 60) {
+    return (Math.round(minutesAgo)).toString() + 'm';
+  } else if (hoursAgo < 24) {
+    return (Math.round(hoursAgo)).toString() + 'h';
+  } else if (daysAgo < 7) {
+    return (Math.round(daysAgo)).toString() + 'd';
+  } else {
+    return (new Date(timestamp)).toDateString();
+  }
 }

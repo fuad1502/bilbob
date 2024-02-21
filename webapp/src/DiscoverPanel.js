023c7528f6cc6a318e2ebf8fd6236115b8663721
Fuad Ismail <fuad1502@gmail.com>
2024-02-21 14:20:18 +0700

    Created SearchBar component

--- webapp/src/DiscoverPanel.js
+++ webapp/src/DiscoverPanel.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "./DiscoverPanel.css"
 import SearchBar from "./SearchBar"
 import SearchResult from "./SearchResult"
 
---
+++ webapp/src/SearchBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./SearchBar.css";
+
+export default function SearchBar() {
+  return (
+    <div id="search-bar">
+      <span>🔎 </span><input type="search"></input>
+    </div>
+  );
+}

31ee0218ff5d35f35cb3b33409a7d0e5c385ec76
Fuad Ismail <fuad1502@gmail.com>
2024-02-21 16:26:15 +0700

    Display search result

--- webapp/src/DiscoverPanel.js
+++ webapp/src/DiscoverPanel.js
@@ -1,13 +1,11 @@
 import React from "react"
 import "./DiscoverPanel.css"
 import SearchBar from "./SearchBar"
-import SearchResult from "./SearchResult"
 
 export default function DiscoverPanel() {
   return (
     <div id="discover-panel">
       <SearchBar />
-      <SearchResult />
     </div>
   )
 }
--- webapp/src/SearchBar.js
+++ webapp/src/SearchBar.js
@@ -1,15 +1,40 @@
 import React from "react";
 import "./SearchBar.css";
 import { getUsers } from "./api-calls";
+import { useState } from "react";
 
 export default function SearchBar() {
-  function handleInput(text) {
-    getUsers(text)
+  const [users, setUsers] = useState([]);
+
+  function handleChange(text) {
+    getUsers(text).then(
+      (result) => {
+        const [users, ok] = result;
+        if (ok) {
+          setUsers(users);
+        }
+      }
+    );
   }
+
+  const searchResults = users.map((user) => {
+    return (
+      <div className="search-result" key={user.username}>
+        {user.name} (@{user.username})
+      </div>
+    );
+  });
+
+  const hasResultClassName = searchResults.length > 0 ? "has-result" : "";
 
   return (
     <div id="search-bar">
-      <span>🔎 </span><input onInput={(e) => handleInput(e.target.value)} type="search"></input>
+      <div id="search-input" className={hasResultClassName}>
+        <span>🔎 </span><input onChange={(e) => handleChange(e.target.value)} type="search"></input>
+      </div>
+      <div id="search-results">
+        {searchResults}
+      </div>
     </div>
   );
 }

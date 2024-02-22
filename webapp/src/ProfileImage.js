50a4df6eb9d33bedb4cac139ea6eaa0d4c4a2198
Fuad Ismail <fuad1502@gmail.com>
2024-02-22 22:50:45 +0700

    Add overlay to profile picture to notify changing profile-picture

--- webapp/src/ProfileImage.js
+++ webapp/src/ProfileImage.js
@@ -6,6 +6,7 @@ export default function ProfileImage({ username, selfUsername, inPost }) {
   const [loading, setLoading] = useState(false);
   const [loaded, setLoaded] = useState(false);
   const [image, setImage] = useState(<img alt=""></img>);
+  const [hideOverlay, setHideOverlay] = useState(true);
   const ref = useRef(null);
 
   if (!loaded && !loading) {
@@ -30,10 +31,33 @@ export default function ProfileImage({ username, selfUsername, inPost }) {
     setProfilePicture(username, e.target.files[0]).then(() => setLoaded(false));
   }
 
-  return (
-    <div id="profile-image" className={inPost? "in-post" : ""} onClick={handleClick}>
-      {image}
-      <input ref={ref} onChange={handleChange} type="file"></input>
-    </div>
-  );
+  function handleMouseEnter() {
+    setHideOverlay(false);
+  }
+
+  function handleMouseLeave() {
+    setHideOverlay(true);
+  }
+
+  const overlay =
+    <div hidden={hideOverlay} id="overlay">
+      <span>Change profile picture</span>
+    </div>;
+
+  if (inPost || username !== selfUsername) {
+    return (
+      <div id="profile-image" className={inPost ? "in-post" : ""} onClick={handleClick} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        {image}
+        <input ref={ref} onChange={handleChange} type="file"></input>
+      </div>
+    );
+  } else {
+    return (
+      <div id="profile-image" className={inPost ? "in-post" : ""} onClick={handleClick} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        {image}
+        {overlay}
+        <input ref={ref} onChange={handleChange} type="file"></input>
+      </div>
+    );
+  }
 }

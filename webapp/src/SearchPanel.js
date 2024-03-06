5f211261089756649df8e8f47c351a118f11810a
Fuad Ismail <fuad1502@gmail.com>
2024-03-06 23:45:44 +0700

    Add MostPopularList to SearchPanel

--- webapp/src/SearchPanel.js
+++ webapp/src/SearchPanel.js
@@ -1,10 +1,13 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import MostPopularList from "./MostPopularList";
+import "./DiscoverPanel.css";
 
 export default function SearchPanel({ onSelectUser }) {
   return (
-    <div id="search-panel" className="main-panel">
+    <div id="discover-items-container" className="in-panel">
       <SearchBar onSelectUser={onSelectUser} inPanel={true} />
+      <MostPopularList onSelectUser={onSelectUser} />
     </div>
   )
 }

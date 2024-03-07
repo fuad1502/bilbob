65104da2cf8a73dfdd2a1ad42453a3cbfecf77d3
Fuad Ismail <fuad1502@gmail.com>
2024-03-07 11:14:23 +0700

    Fixed bug: posts should be cleared if switching profile panels

--- webapp/src/ProfilePanel.js
+++ webapp/src/ProfilePanel.js
@@ -24,6 +24,7 @@ export default function ProfilePanel({ username, selfUsername }) {
   }
 
   if (loadedPosts && username !== profileInfo.username) {
+    setPosts([]);
     setLoadedPosts(false);
   }
 

4ed81e5bba24c99e3d682b8eb7c3433291833330
Fuad Ismail <fuad1502@gmail.com>
2024-03-07 20:41:58 +0700

    Add bottomed out check, if already bottomed out, that is, no more earlier posts, stop requests; Add postDate as key to each Post

--- webapp/src/PostPanel.js
+++ webapp/src/PostPanel.js
@@ -1,36 +1,47 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 import PostSubmitForm from "./PostSubmitForm"
 import Posts from "./Posts"
 import { getPosts } from "./api-calls";
 
 export default function PostPanel({ username, onSelectUser }) {
   const [posts, setPosts] = useState([]);
+  const [loaded, setLoaded] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [bottomed, setBottomed] = useState(false);
 
-  useEffect(() => {
-    if (!loading) {
-      setLoading(true);
-      let fromTimestamp = "";
-      if (posts.length > 0) {
-        fromTimestamp = posts[posts.length - 1].postDate;
-      }
-      getPosts(username, true, fromTimestamp).then((result) => {
-        const [returnedPosts, _] = result;
-        setPosts(posts.concat(returnedPosts));
-      });
+  if (!bottomed && !loaded && !loading) {
+    setLoading(true);
+    let fromTimestamp = "";
+    if (posts.length > 0) {
+      fromTimestamp = posts[posts.length - 1].postDate;
     }
-  }, [loading]);
+    getPosts(username, true, fromTimestamp).then((result) => {
+      const [returnedPosts, ok] = result;
+      if (ok) {
+        if (returnedPosts.length > 0) {
+          setPosts(posts.concat(returnedPosts));
+        } else {
+          setBottomed(true);
+        }
+      }
+      setLoading(false);
+      setLoaded(true);
+    });
+  }
 
   function handleSubmit() {
     setPosts([]);
-    setLoading(false);
+    setBottomed(false);
+    setLoaded(false);
   }
 
-  window.onscroll = function(_e) {
-    if ((window.innerHeight * 1.2 + Math.round(window.scrollY)) >= document.body.offsetHeight) {
-      setLoading(false);
-    }
-  };
+  if (!bottomed) {
+    window.onscroll = function(_e) {
+      if ((window.innerHeight * 1.2 + Math.round(window.scrollY)) >= document.body.offsetHeight) {
+        setLoaded(false);
+      }
+    };
+  }
 
   return (
     <div id="post-panel" className="main-panel">

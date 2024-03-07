c3b40dc9c4796eec8818ece0f6fc09be5e9d1ea4
Fuad Ismail <fuad1502@gmail.com>
2024-03-07 10:02:05 +0700

    Add gopher image and 'under construction' message to MarketPanel

--- webapp/src/MarketPanel.js
+++ webapp/src/MarketPanel.js
@@ -1,7 +1,12 @@
 import React from "react";
+import gopher from "./1024px-Go_gopher_pencil_wrench.jpg";
+import "./MarketPanel.css";
 
 export default function MarketPanel() {
   return (
-    <div id="market-panel" className="main-panel">MarketPanel</div>
+    <div id="market-panel" className="main-panel">
+      <h2>Under construction 🚧</h2>
+      <img src={gopher} alt="Golang's gopher constructing something good" />
+    </div>
   );
 }
